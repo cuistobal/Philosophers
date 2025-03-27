002639e2d3d94e7f9db854f79e0feb1216058a13
chrleroy <you@example.com>
2025-03-27 16:05:43 +0100

    aled3 apm3 2703 PAUL

---
+++ mandatory/includes/global_variables.h
@@ -0,0 +1,37 @@
+#ifndef GLOBAL_VARIABLES_H
+# define GLOBAL_VARIABLES_H
+
+//Table macros
+# define TABLES 6
+
+# define STS 0
+# define CNT 1
+# define EAT 2
+# define SLP 3
+# define DIE 4
+# define END 5
+
+//Philo macros
+# define STATS 3
+
+# define POSTN 0	 
+# define LMEAL 1
+# define EATEN 2
+
+//Routine messages
+# define THNK "is thinking\n"
+# define FORK "has taken a fork\n"
+# define EATS "is eating\n"
+# define SLEP "is sleeping\n"
+# define DIED "died\n"
+
+//Error messages
+# define ARGC "Usage: ./philosophers philos die eat sleep [min eat].\n"
+# define ATOI "Invalid parameter.\n"
+# define THREAD_CREATE "Failed to create Thread.\n"
+# define INIT_TABLE "Failed to initialise the simulaiton.\n"
+
+//Misc
+# define SUCCESS "Greaaaaat suucceeeeeees.\n"
+# define MSEC 1000
+#endif
---
+++ mandatory/includes/standard_headers.h
@@ -0,0 +1,13 @@
+#ifndef STANDARD_HEARDERS_H
+# define STANDARD_HEARDERS_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+#endif
