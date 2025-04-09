88a3eb13ebf82a7c80d2e34b137f0e9a26eb1416
chrleroy <you@example.com>
2025-04-09 09:07:57 +0200

    Modif structure - mat1 0904 PAUL

---
+++ includes/global_variables.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   global_variables.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/29 12:07:20 by chrleroy          #+#    #+#             */
+/*   Updated: 2025/04/03 09:11:20 by chrleroy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GLOBAL_VARIABLES_H
+# define GLOBAL_VARIABLES_H
+
+//Table macros
+# define TABLES 6
+
+# define STS 0
+# define CNT 1
+# define DIE 2
+# define EAT 3
+# define SLP 4
+# define END 5
+
+//Philo macros
+# define STATS 4
+
+# define POSTN 0
+# define LMEAL 1
+# define EATEN 2
+# define START 3
+
+//Routine messages
+# define THNK "is thinking\n"
+# define FORK "has taken a fork\n"
+# define EATS "is eating\n"
+# define SLEP "is sleeping\n"
+# define DIED "died\n"
+
+//Error messages
+# define ARGC "Usage: ./philosophers philos die eat sleep [min eat].\n"
+# define ATOI "Invalid parameter.\n"
+# define THREAD_CREATE "Failed to create Thread.\n"
+# define INIT_TABLE "Failed to initialise the simulaiton.\n"
+
+//Misc
+# define SUCCESS "Greaaaaat suucceeeeeees.\n"
+# define MSEC 1000
+# define TCAP 10000
+
+#endif
