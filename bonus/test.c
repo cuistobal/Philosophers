f4c367789e2bc71467fefc6048eda75405e8bd97
chrleroy <you@example.com>
2025-04-09 12:56:33 +0200

    Modif forks order :( - mat1 0904 PAUL

---
+++ bonus/test.c
@@ -0,0 +1,10 @@
+
+int main(int argc, char **argv)
+{
+	int	count;
+
+	if (argc > 1)
+	{
+		count = atoi(argv[1]);	
+	}
+}
--- mandatory/sources/philo_sub_routines.c
+++ mandatory/sources/philo_sub_routines.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 12:07:20 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/04/09 10:31:14 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/04/09 12:40:04 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,6 @@ void	thinking(t_phil	*philosopher)
 	think = 1;
 	if (the_show_must_go_on(philosopher))
 	{
-		/*
-		pthread_mutex_lock(&philosopher->clock);
-		if (get_timestamp() - philosopher->stats[LMEAL] > \
-				philosopher->table->params[DIE] )
-			think = ;
-		pthread_mutex_unlock(&philosopher->clock);		
-		*/
 		status(philosopher, THNK);
 		my_usleep(philosopher, think, get_timestamp());
 	}
