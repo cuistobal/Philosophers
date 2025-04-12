c60dd03fd15cfea33af6f95f65eff554ff1eb8e9
chrleroy <you@example.com>
2025-04-12 14:32:27 +0200

    Handle signals - apm3 1204 PAUL

--- utilities/wait_pid.c
+++ utilities/wait_pid.c
@@ -6,7 +6,7 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 12:38:26 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/04/12 14:08:16 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/04/12 14:32:15 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,40 +37,31 @@
 //
 //Sending and *index to take kill processes in case we get a wrong exti status
 //void	waiter(t_tabl *table, int *index, bool *success)
-void	waiter(pid_t *pids, int *index, bool *success, int count)
+bool	waiter(pid_t *pids, int count)
 {
+	bool	end;
+	int		index;
 	int		status;
+	int		finished;
 
-//	while (*index < table->params[CNT])
-	while (*index < count)
+	index = 0;
+	finished = 0;
+	while (index < count)
 	{
-		if (!*success)
+		waitpid(pids[index], &status, WNOHANG);
+		if (WIFEXITED(status))
 		{
-			//kill(table->pids[*index], DEAD);
-			if (kill(pids[*index], DEATH) == 0)
-				printf("Killed process %d.\n", *index);
-		}
-		else
-		{
-		//	waitpid(table->pids[index], &status, 0)
-			waitpid(pids[*index], &status, 0);
-			if (WIFEXITED(status) && WEXITSTATUS(status) == DEATH)
-			{
-			//	status(table->philosopher[*index], DIED);
-				printf("process %d died with status %d.\n", *index, status);	
-				*success = false;
-				*index = 0;
-			//	waiter(table, index, success);
-				waiter(pids, index, success, count);
-			}
-			else if (WIFEXITED(status) && WEXITSTATUS(status) == FULL)
-			//	kill(table->pids[*index], FULL);
-				kill(pids[*index], FULL);
+			if (WEXITSTATUS(status) == DEATH)	
+				printf("process %d died with status %d.\n", index, status);
+			else if (WEXITSTATUS(status) == FULL)
+				printf("process %d died with status %d.\n", index, status);
 			else
-				printf("%d	->	Process %d is over with status %d\n", *index, pids[*index], status);
+				printf("%d	->	Process %d is over with status %d\n", index, pids[index], status);
+			finished++;	
 		}
-		(*index)++;
+		index++;
 	}
+	return (finished == count);
 }
 
 void	create_process(pid_t *pids,	int index)
@@ -81,9 +72,7 @@ void	create_process(pid_t *pids,	int index)
 	if (current > 0)
 		pids[index] = current;
 	else if (current == 0)
-		index == 7 ? exit(DEATH) : index == 4 ? exit(FULL) : exit(SUCES);
-	else
-		printf("FAILED\n");
+		index == 7 ? (usleep(10000), exit(DEATH)) : index == 4 ? (usleep(1000), exit(FULL)) : (usleep(100), exit(SUCES));
 }
 
 //test
@@ -92,7 +81,6 @@ int main(int argc, char **argv)
 	pid_t	*pids;
 	pid_t	current;
 	int		index = 0;
-	bool sucess = true;
 
 	if (argc == 2)
 	{
@@ -106,7 +94,7 @@ int main(int argc, char **argv)
 			create_process(pids, index);
 			index++;
 		}
-		index = 0;
-		waiter(pids, &index, &sucess, count);
+		while (!waiter(pids, count))
+			continue ;
 	}
 }
