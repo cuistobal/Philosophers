491d1c8c66de928d033c9aef6a05aec8df2750c6
chrleroy <you@example.com>
2025-04-14 11:58:23 +0200

    Press F - le pc va imploser - mat4 1304 PAUL

--- includes/bonus/utilities_bonus.h
+++ includes/bonus/utilities_bonus.h
@@ -6,14 +6,20 @@
 /*   By: chrleroy <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 15:38:31 by chrleroy          #+#    #+#             */
-/*   Updated: 2025/04/14 11:48:29 by chrleroy         ###   ########.fr       */
+/*   Updated: 2025/04/14 11:55:20 by chrleroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILITIES_BONUS_H
 # define UTILITIES_BONUS_H
 
-# include "utilities.h"
+void	status(t_phil *philo, char *status);
+
+void	my_usleep(t_phil *philo, long sleep, long start);
+
+int		my_atoi(char *argv);
+
+long	get_timestamp(void);
 
 bool	the_sh0w_must_go_on(t_tabl *table);
 
